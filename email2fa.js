1c2d04f3245f6e36450e10cb51d440edadbcfa85
Batman190803 <svatik.bs@gmail.com>
2025-12-06 23:57:50 +0200

    Zminu22

--- email2fa.js
+++ email2fa.js
@@ -3,7 +3,6 @@ const axios = require("axios");
 const crypto = require("crypto");
 
 function generate2FACode() {
-  // 6-значний код
   return String(Math.floor(100000 + Math.random() * 900000));
 }
 
@@ -11,23 +10,24 @@ function hashCode(code) {
   return crypto.createHash("sha256").update(code).digest("hex");
 }
 
-async function send2FACodeEmail(toEmail, code) {
-  const token = process.env.MAILTRAP_TOKEN;
-  const fromEmail =
-    process.env.MAILTRAP_FROM_EMAIL || "mailtrap@demomailtrap.io";
+const MAILTRAP_TOKEN = process.env.MAILTRAP_TOKEN;
+const MAILTRAP_FROM_EMAIL =
+  process.env.MAILTRAP_FROM_EMAIL || "mailtrap@demomailtrap.io";
+const MAILTRAP_FROM_NAME = process.env.MAILTRAP_FROM_NAME || "TechNest";
 
-  if (!token) {
-    console.error("[Mailtrap] MAILTRAP_TOKEN is not set");
-    throw new Error("MAILTRAP_TOKEN не налаштований");
+async function send2FACodeEmail(toEmail, code) {
+  if (!MAILTRAP_TOKEN) {
+    console.error("MAILTRAP_TOKEN is not set");
+    throw new Error("Mail service not configured");
   }
 
   try {
-    const response = await axios.post(
+    const resp = await axios.post(
       "https://send.api.mailtrap.io/api/send",
       {
         from: {
-          email: fromEmail,
-          name: "TechNest",
+          email: MAILTRAP_FROM_EMAIL,
+          name: MAILTRAP_FROM_NAME,
         },
         to: [
           {
@@ -39,14 +39,13 @@ async function send2FACodeEmail(toEmail, code) {
       },
       {
         headers: {
-          // ❗ Головне: використовуємо саме Api-Token
-          "Api-Token": token,
+          Authorization: `Bearer ${MAILTRAP_TOKEN}`,
           "Content-Type": "application/json",
         },
       }
     );
 
-    console.log("[Mailtrap] Email sent:", response.data);
+    console.log("Mailtrap API OK", resp.data);
   } catch (err) {
     console.error(
       "Mailtrap API error",
